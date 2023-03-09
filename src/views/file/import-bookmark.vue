d5034617d87ce6333e29deae26767c1305a50387
potatomatos <782399171@qq.com>
2023-03-09 18:25:51 +0800

    文件上传处理

--- src/views/file/import-bookmark.vue
+++ src/views/file/import-bookmark.vue
@@ -8,17 +8,56 @@
       accept="text/html"
       :limit="1"
       drag
-      action="https://jsonplaceholder.typicode.com/posts/">
+      action="/api/bookmark/my-bookmark/import/bookmark"
+      :headers="{access_token:token }"
+      :data="config">
       <i class="el-icon-upload"></i>
       <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
       <div class="el-upload__tip" slot="tip">只能上传html文件，且不超过500kb</div>
     </el-upload>
+    <el-form ref="form" :model="config">
+      <el-form-item>
+        <el-checkbox v-model="config.clearFlag" true-label="1" false-label="0">导入前清空现有收藏夹</el-checkbox>
+        <el-checkbox v-model="config.newFolderFlag" true-label="1" false-label="0">为导入的收藏新建一个文件夹</el-checkbox>
+      </el-form-item>
+      <el-form-item>
+        <el-progress :text-inside="true" :stroke-width="24" :percentage="100" :color="customColors"></el-progress>
+      </el-form-item>
+      <el-form-item>
+        <el-button size="small" type="success" @click="submitUpload">导入</el-button>
+      </el-form-item>
+      </el-form>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'import-bookmark'
+  name: 'import-bookmark',
+  data () {
+    return {
+      token: '',
+      config: {
+        clearFlag: '0',
+        newFolderFlag: '0'
+      },
+      // 进度条颜色
+      customColors: [
+        {color: '#f56c6c', percentage: 20},
+        {color: '#e6a23c', percentage: 40},
+        {color: '#5cb87a', percentage: 60},
+        {color: '#1989fa', percentage: 80},
+        {color: '#6f7ad3', percentage: 100}
+      ]
+    }
+  },
+  created () {
+    this.token = localStorage.getItem('token')
+  },
+  methods: {
+    submitUpload () {
+      this.$refs.upload.submit()
+    }
+  }
 }
 </script>
 
