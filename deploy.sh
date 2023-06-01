8b761ea5f45157cdf4e7b76e7a438af6d8ad3600
mengjinyuan <782399171@qq.com>
2023-06-01 23:19:45 +0800

    添加部署脚本

--- deploy.sh
+++ deploy.sh
@@ -7,13 +7,13 @@ yarn install
 yarn build
 echo "开始构建镜像..."
 echo "停止容器"
-docker stop bookmark-vue || true
+sudo docker stop bookmark-vue || true
 echo "删除容器"
-docker rm bookmark-vue || true
+sudo docker rm bookmark-vue || true
 echo "删除镜像"
-docker rmi bookmark-vue || true
+sudo docker rmi bookmark-vue || true
 echo "打包镜像"
-docker build -t bookmark-vue .
+sudo docker build -t bookmark-vue .
 echo "镜像打包成功！开始运行容器"
-docker run -d -p 8003:80 --name bookmark-vue bookmark-vue
+sudo docker run -d -p 8003:80 --name bookmark-vue bookmark-vue
 echo "容器已启动！"
