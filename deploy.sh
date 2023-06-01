f431af108d2b409b2465dc1ba06f04b5b14d7e82
mengjinyuan <782399171@qq.com>
2023-06-01 23:05:02 +0800

    添加部署脚本

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+echo "开始打包项目..."
+node -v
+npm -v
+npm config set registry https://registry.npm.taobao.org
+npm config get registry
+npm install
+npm run build
+echo "开始构建镜像..."
+echo "停止容器"
+docker stop bookmark-vue || true
+echo "删除容器"
+docker rm bookmark-vue || true
+echo "删除镜像"
+docker rmi bookmark-vue || true
+echo "打包镜像"
+docker build -t bookmark-vue .
+echo "镜像打包成功！开始运行容器"
+docker run -d -p 8003:80 --name bookmark-vue bookmark-vue
+echo "容器已启动！"
